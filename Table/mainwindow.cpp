ec83da2ecf21cbe0c4249abf6f22ac5ae8d3adfb
arturjkeee <113170667+arturjkeee@users.noreply.github.com>
2023-03-17 13:59:19 +0300

    Add Table

---
+++ Table/mainwindow.cpp
@@ -0,0 +1,63 @@
+#include <QtGui>
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+//#include "spreadSheet.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    //spreadSheet = new SpreadSheet;
+    //setCentralWidget(spreadSheet);
+
+    connect(ui->actionAbout_Qt, SIGNAL(triggered()), qApp, SLOT(aboutQt()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+void MainWindow::newFile()
+{
+
+}
+void MainWindow::open()
+{
+
+}
+bool MainWindow::save()
+{
+    return true;
+}
+bool MainWindow::saveAs()
+{
+    return true;
+}
+void MainWindow::find()
+{
+
+}
+
+void MainWindow::goToCell()
+{
+
+}
+
+void MainWindow::about()
+{
+
+}
+void MainWindow::aboutQt()
+{
+
+}
+
+void MainWindow::sort()
+{
+
+}
+
+
+
